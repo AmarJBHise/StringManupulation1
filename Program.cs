c475267712744262519d3ea33dfb28660ffd3458
bhiseamar811@gmail.com <bhiseamar811@gmail.com>
2024-02-02 11:02:46 +0530

    added a programe of caculte leanth of the string

--- Program.cs
+++ Program.cs
@@ -1,20 +1,36 @@
-﻿namespace StringManupulation
+﻿using System.Diagnostics.Metrics;
+
+namespace StringManupulation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string FirstName, LastName, FullName;
+            //string FirstName, LastName, FullName;
 
-            Console.WriteLine("Please enter the firstname");
-            FirstName= Console.ReadLine();
+            //Console.WriteLine("Please enter the firstname");
+            //FirstName= Console.ReadLine();
 
-            Console.WriteLine("please enter the last name");
-            LastName= Console.ReadLine();   
+            //Console.WriteLine("please enter the last name");
+            //LastName= Console.ReadLine();   
 
-            FullName= FirstName+" "+ LastName;
+            //FullName= FirstName+" "+ LastName;
 
-            Console.WriteLine(FullName);
+            //Console.WriteLine(FullName);
+
+            // calcute length of the string
+
+            Console.WriteLine("Please enter any string");
+
+            string Str=Console.ReadLine();
+
+             int Counter = 0;
+
+            foreach (var item in Str)
+            {
+                Counter++;
+            }
+            Console.WriteLine("leanth of the string is =" + Counter);
 
         }
 
